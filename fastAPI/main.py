0d175dcad64f795d15cde5f6ae0b7bd11a14a143
ShimamuraMiu <simamura@primagest.co.jp>
2023-04-06 17:00:25 +0900

    SCRF機能追加

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+from fastapi.middleware.cors import CORSMiddleware
 # from typing import List
 
 import models
@@ -12,6 +16,30 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(route_todo.router)
 app.include_router(route_auth.router)
+
+origins = ['http://localhost:3000', 'https://fastapi-1436a.web.app']
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@CsrfProtect.load_config
+def get_csrf_config():
+    return schemas.CsrfSettings()
+
+
+@app.exception_handler(CsrfProtectError)
+def csrf_protect_exception_handler(request: Request, exc: CsrfProtectError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={'detail':  exc.message}
+    )
 
 
 @app.get("/", response_model=schemas.SuccessMsg)  # エンドポイントのパスを定義？
--- fastAPI/routers/route_todo.py
+++ fastAPI/routers/route_todo.py
@@ -2,15 +2,16 @@
 # エンドポイントの親？ #
 #######################
 
-from fastapi import Response, HTTPException  # 型をインポート
+from fastapi import Response, Request, HTTPException  # 型をインポート
 from fastapi import APIRouter, Depends                # 型をインポート
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_201_CREATED         # ステータス上書き用
+from fastapi.encoders import jsonable_encoder         # jsonからdictに変換
+from fastapi_csrf_protect import CsrfProtect
 
 from database import SessionLocal
 import schemas                                        # schemasファイルからインポート
 import crud                                           # crudファイルをインポート
-from starlette.status import HTTP_201_CREATED         # ステータス上書き用
-from fastapi.encoders import jsonable_encoder         # jsonからdictに変換
 from auth_utils import AuthJwtCsrf
 
 # インスタンス化
@@ -31,9 +32,12 @@ def get_db():
 
 # INSERT
 @router.post("/api/todo", response_model=schemas.Todo)  # 型の指定（response_model）
-async def create_todo(response: Response, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+async def create_todo(request: Request, response: Response, todo: schemas.TodoCreate,
+                      db: Session = Depends(get_db), csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(request, csrf_protect, request.headers)
     res = await crud.create_todo(db=db, todo=todo)
     response.status_code = HTTP_201_CREATED
+    response.set_cookie(key="access_token", value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True)
     if res:
         return res
     raise HTTPException(status_code=404, detail="Create task failed")
@@ -41,15 +45,18 @@ async def create_todo(response: Response, todo: schemas.TodoCreate, db: Session 
 
 # SELECT ALL
 @router.get("/api/todo/", response_model=list[schemas.Todo])
-async def get_todos(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def get_todos(request: Request, offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    auth.verify_jwt(request)
     res = await crud.get_todos(db, offset=offset, limit=limit)
     return res
 
 
 # SELECT（データ1つ）
 @router.get("/api/todo/{todo_id}", response_model=schemas.Todo)
-async def get_single_todo(todo_id: int, db: Session = Depends(get_db)):
+async def get_single_todo(request: Request, response: Response, todo_id: int, db: Session = Depends(get_db)):
+    new_token, _ = auth.verify_update_jwt(request)
     res = await crud.get_todo(db, todo_id=todo_id)
+    response.set_cookie(key="access_token", value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True)
     if res:
         return res
     raise HTTPException(status_code=404, detail=f"Task of ID: {todo_id} doesn't exist")
@@ -57,9 +64,12 @@ async def get_single_todo(todo_id: int, db: Session = Depends(get_db)):
 
 # UPDATE
 @router.put("/api/todo/{todo_id}", response_model=schemas.Todo)
-async def update_todo(todo_id: int, data: schemas.TodoBody, db: Session = Depends(get_db)):
+async def update_todo(request: Request, response: Response, todo_id: int, data: schemas.TodoBody,
+                      db: Session = Depends(get_db), csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(request, csrf_protect, request.headers)
     todo = jsonable_encoder(data)
     res = await crud.update_todo(db=db, id=todo_id, data=todo)
+    response.set_cookie(key="access_token", value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True)
     if res is None:
         raise HTTPException(status_code=404, detail="Update task failed")
     return res
@@ -67,8 +77,11 @@ async def update_todo(todo_id: int, data: schemas.TodoBody, db: Session = Depend
 
 # DELETE
 @router.delete("/api/todo/{todo_id}", response_model=schemas.SuccessMsg)
-async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+async def delete_todo(request: Request, response: Response, todo_id: int,
+                      db: Session = Depends(get_db), csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(request, csrf_protect, request.headers)
     res = await crud.delete_todo(db=db, id=todo_id)  # T or F
+    response.set_cookie(key="access_token", value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True)
     if res:
         return {'message': 'Successfully deleted'}
     raise HTTPException(status_code=404, detail="Delete task failed")
