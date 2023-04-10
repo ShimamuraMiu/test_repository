74cb0ad2e639acf637e3636481bdd76ca64972ca
ShimamuraMiu <simamura@primagest.co.jp>
2023-04-10 13:56:12 +0900

    first commit

---
+++ react_farm/src/component_temp/Todo.tsx
@@ -0,0 +1,19 @@
+import {VFC} from 'react'
+import { LogoutIcon } from '@heroicons/react/outline'
+import { useProcessAuth } from '../hooks/useProcessAuth'
+
+export const Todo:VFC = () => {
+  const { logout } = useProcessAuth()
+
+  return (
+    // 中央寄せ、フォント等の指定
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      // logoutアイコンの設置
+      <LogoutIcon
+      // 上記でカスタムフックから読み込んだlogoutの関数を呼び出す
+        onClick={logout}
+        className="h-7 w-7 mt-1 mb-5 text-blue-500 cursor-pointer"
+      />
+    </div>
+  )
+}
